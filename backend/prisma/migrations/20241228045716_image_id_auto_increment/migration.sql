aa3a472ec8d33c9eb72c7c0e0458a8bb79a2f368
ggred0123 <ggred0123@gmail.com>
2024-12-29 19:53:48 +0900

    프론트에서 클라우드 이미지 링크 받기

--- backend/prisma/migrations/20241228045716_image_id_auto_increment/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-CREATE SEQUENCE image_image_id_seq;
-ALTER TABLE "image" ALTER COLUMN "image_id" SET DEFAULT nextval('image_image_id_seq');
-ALTER SEQUENCE image_image_id_seq OWNED BY "image"."image_id";
